71889412610e48c4b1d155c05c163f0276eb0064
bartoszrudzinski <bartosz.rudzinski@infermedica.com>
2022-11-21 14:23:06 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ElTable, ElTableColumn } from "element-plus";
+
+const tableData = [
+  {
+    date: "2016-05-03",
+    name: "Christopher",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-02",
+    name: "Christopher",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-04",
+    name: "Christopher",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-01",
+    name: "Christopher",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+];
+</script>
+
+<template>
+  <el-table :data="tableData" border style="width: 100%">
+    <el-table-column prop="date" label="Date" width="90" />
+    <el-table-column prop="name" label="Name" width="90" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+</template>
+
+<style scoped></style>
